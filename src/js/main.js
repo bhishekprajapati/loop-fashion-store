2ecb5a669dac034fdbb4c5a72ba0799af5f69b47
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-10-22 20:28:25 +0530

    feat: add smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
+import "../locomotive.css";
+import LocomotiveScroll from "locomotive-scroll";
 
+const locomotiveScroll = new LocomotiveScroll({
+  lenisOptions: {
+    wrapper: window,
+    content: document.documentElement,
+    duration: 2,
+    orientation: "vertical",
+    gestureOrientation: "vertical",
+    smoothWheel: true,
+    wheelMultiplier: 0.5,
+    touchMultiplier: 2,
+    normalizeWheel: true,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  },
+});
