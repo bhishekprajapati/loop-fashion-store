3334581722ffdf771f00d100d01cd48974ef1000
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-10-26 20:05:36 +0530

    refactor

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,7 +1,16 @@
+import "../locomotive.css";
+import LocomotiveScroll from "locomotive-scroll";
+
 import MarqueeOffers from "../components/MarqueeOffers";
 import CompanyLogoDisplay from "../components/CompanyLogoDisplay";
 import BrandDescription from "../components/BrandDescription";
 import CollectionSection from "../components/CollectionSection";
+
+function round(number, places = 2) {
+  const multiplier = Math.pow(10, places);
+  number *= multiplier;
+  return Math.floor(number) / multiplier;
+}
 
 export default {
   components: {
@@ -9,6 +18,46 @@ export default {
     CompanyLogoDisplay,
     BrandDescription,
     CollectionSection,
+  },
+  mounted() {
+    const locomotiveScroll = new LocomotiveScroll({
+      lenisOptions: {
+        wrapper: window,
+        content: document.documentElement,
+        duration: 2,
+        orientation: "vertical",
+        gestureOrientation: "vertical",
+        smoothWheel: true,
+        wheelMultiplier: 0.5,
+        touchMultiplier: 2,
+        normalizeWheel: true,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      },
+    });
+
+    window.addEventListener("horizontalParallaxEvent", (e) => {
+      const { target, progress } = e.detail;
+      const translate = (1 - progress) * 150;
+      target.style.transform = `translateX(${translate}px)`;
+    });
+
+    window.addEventListener("rotateEvent", (e) => {
+      const { target, progress } = e.detail;
+      target.style.transform = `rotate(${
+        progress * target.dataset.scrollRotateMax
+      }deg)`;
+    });
+
+    window.addEventListener("gradientFillEvent", (e) => {
+      const { target, progress } = e.detail;
+      const targetPropName = target.dataset.scrollTargetProp;
+      const targetPropValue = round(progress * 100 * 4);
+      target.style.setProperty(`--${targetPropName}`, `${targetPropValue}%`);
+    });
+
+    document
+      .getElementById("scroll-to-top")
+      .addEventListener("click", (e) => locomotiveScroll.scrollTo("body"));
   },
   template: `
     <header>
--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,5 @@
-import "../locomotive.css";
-import LocomotiveScroll from "locomotive-scroll";
 import Home from "./Home";
 
-function round(number, places = 2) {
-  const multiplier = Math.pow(10, places);
-  number *= multiplier;
-  return Math.floor(number) / multiplier;
-}
-
-const locomotiveScroll = new LocomotiveScroll({
-  lenisOptions: {
-    wrapper: window,
-    content: document.documentElement,
-    duration: 2,
-    orientation: "vertical",
-    gestureOrientation: "vertical",
-    smoothWheel: true,
-    wheelMultiplier: 0.5,
-    touchMultiplier: 2,
-    normalizeWheel: true,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  },
-});
-
-window.addEventListener("horizontalParallaxEvent", (e) => {
-  const { target, progress } = e.detail;
-  const translate = (1 - progress) * 150;
-  target.style.transform = `translateX(${translate}px)`;
-});
-
-window.addEventListener("rotateEvent", (e) => {
-  const { target, progress } = e.detail;
-  target.style.transform = `rotate(${
-    progress * target.dataset.scrollRotateMax
-  }deg)`;
-});
-
-window.addEventListener("gradientFillEvent", (e) => {
-  const { target, progress } = e.detail;
-  const targetPropName = target.dataset.scrollTargetProp;
-  const targetPropValue = round(progress * 100 * 4);
-  target.style.setProperty(`--${targetPropName}`, `${targetPropValue}%`);
-});
-
-document
-  .getElementById("scroll-to-top")
-  .addEventListener("click", (e) => locomotiveScroll.scrollTo("body"));
-
-const { createApp, ref } = Vue;
+const { createApp } = Vue;
 const app = createApp(Home);
 app.mount("#app");
